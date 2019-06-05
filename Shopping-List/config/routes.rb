149a53b99dd98c0f155bdb95078ae4dc81c6cfb3
Jon <jhharwood88@gmail.com>
2019-06-05 15:48:34 -0400

    added nav bar to layout, added home route

--- Shopping-List/config/routes.rb
+++ Shopping-List/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :shoppinglists
-  resources :items
-  resources :users
+  # resources :shoppinglists
+  # resources :items
+  # resources :users
+  
+  # devise_for :users, :paths => 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'application#welcome'
+  # resource :user do
+    # resources :itmes, only: [:index, :new, :create]
+  # end
 end
+# 
