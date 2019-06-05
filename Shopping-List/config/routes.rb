31694414d5fd2e7da50241305d22538ff963b95a
Jon <jhharwood88@gmail.com>
2019-06-05 16:07:58 -0400

    removed older user files, generated new files using devise

--- Shopping-List/config/routes.rb
+++ Shopping-List/config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  # resources :shoppinglists
-  # resources :items
-  # resources :users
-  
-  # devise_for :users, :paths => 'users'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, :paths => 'users'
+ 
   root to: 'application#welcome'
-  # resource :user do
-    # resources :itmes, only: [:index, :new, :create]
-  # end
+  resource :user do
+    resources :itmes, only: [:index, :new, :create]
+  end
 end
-# 
--- Shopping-List/db/schema.rb
+++ Shopping-List/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_001822) do
+ActiveRecord::Schema.define(version: 2019_06_05_200055) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -26,8 +26,15 @@ ActiveRecord::Schema.define(version: 2019_06_04_001822) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
