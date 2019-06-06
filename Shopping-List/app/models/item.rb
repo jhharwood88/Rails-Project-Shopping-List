483f4774c8b2644bf79b5c273870a3bcd25d609e
Jon <jhharwood88@gmail.com>
2019-06-06 19:06:24 -0400

    added validations to shoppinglist

--- Shopping-List/app/models/item.rb
+++ Shopping-List/app/models/item.rb
@@ -3,15 +3,5 @@ class Item < ApplicationRecord
 	belongs_to :itemlist
   	has_many :users, through: :shoppinglists
 
+  	
 end
-
-
-#include validations for item model, validate presence on name for shopping list
-
-#devise is acceptable for omniauth 
-
-#remove quantity from item, item list has item id and quantity 
-
-#new join on item list, shopping list is no longer a join table
-	#a shopping list is always associated with a user, creating new list associates items with list through itemid.
-
--- Shopping-List/app/models/shoppinglist.rb
+++ Shopping-List/app/models/shoppinglist.rb
@@ -1,4 +1,7 @@
 class Shoppinglist < ApplicationRecord
 	belongs_to :user
 	has_many :items, through: :item_lists
+
+	validates :name, presence: true
+	validates :user_id, presence: true
 end
