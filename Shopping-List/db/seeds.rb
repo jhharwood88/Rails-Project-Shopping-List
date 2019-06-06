e59c63e453a6fa15ec0187b58516af73739d31f7
Jon <jhharwood88@gmail.com>
2019-06-06 18:36:41 -0400

    added seeds for items table

---
+++ Shopping-List/db/seeds.rb
@@ -0,0 +1,6 @@
+
+Item.create(name: "Chicken", category: "Meat", price: 5.00 )
+Item.create(name: "Apple", category: "Fruit", price: 0.70)
+Item.create(name: "Soda", category: "Drink", price: 1.50)
+Item.create(name: "Soap", category: "Cleaning", price: 1.30)
+Item.create(name: "Beef", category: "Meat", price: 7.00)
