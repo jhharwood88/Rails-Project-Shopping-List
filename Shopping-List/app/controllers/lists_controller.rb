19f1235d185f302e60079f8811849e7bc25733a4
Jon <jhharwood88@gmail.com>
2019-06-07 13:54:49 -0400

    added new and create method on listcontroller, added list params

--- Shopping-List/app/controllers/lists_controller.rb
+++ Shopping-List/app/controllers/lists_controller.rb
@@ -6,5 +6,21 @@ class ListsController < ApplicationController
 		
 	end
 
+	def new
+		@list = List.new 
+	end
+
+	def create
+		@list = List.create(list_params)
+		if @list
+			redirect_to list_path(@list)
+		end
+	end
+
+	private
+
+	  def list_params
+	    params.require(:list).permit(:name, :user_id)
+	  end
 
 end
