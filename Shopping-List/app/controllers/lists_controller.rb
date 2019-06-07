1e602cef95b2f5f9ac062eb48e69eed5366b0c7d
Jon <jhharwood88@gmail.com>
2019-06-07 13:53:52 -0400

    added link to lists from homepage, link to create new list on lists page

--- Shopping-List/app/controllers/lists_controller.rb
+++ Shopping-List/app/controllers/lists_controller.rb
@@ -4,18 +4,7 @@ class ListsController < ApplicationController
 
 	def index 
 		
-	
 	end
 
-	# def create
- #    cart = Shoppinglist.create(name: params[:name], user_id: current_user.id)
- #    if cart
- #      response = ride.take_ride
-      
- #      redirect_to user_path(cart.user_id)
- #    else
- #    	flash[:notice] = "Could not create a new list"
- #      redirect_to user_path(ride.attraction)
- #    end
- #  end
+
 end
--- Shopping-List/app/models/list.rb
+++ Shopping-List/app/models/list.rb
@@ -3,8 +3,4 @@ class List < ApplicationRecord
   has_many :item_lists
   has_many :items, through: :item_lists
 
-  def is_empty?
-  	@all_lists = List.all
-
-  end
 end
