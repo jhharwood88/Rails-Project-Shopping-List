37c47335d19b6d61a2c7b85192e66dfa19b436cf
Jon <jhharwood88@gmail.com>
2019-06-08 15:30:56 -0400

    added validations for item name on creation, and item_id on adding to a list

--- Shopping-List/app/models/item.rb
+++ Shopping-List/app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   	has_many :lists, through: :item_lists
 
   	validates :name, :category, :price, presence: true
+  	validates :name, uniqueness: true
 end
 
 #validates presence of for name cat price
--- Shopping-List/app/models/item_list.rb
+++ Shopping-List/app/models/item_list.rb
@@ -4,5 +4,6 @@ class ItemList < ApplicationRecord
 	belongs_to :item
 
 	validates :item_id, presence: true
+	validates :item_id, uniqueness: true
 	validates :quantity, presence: true
 end
