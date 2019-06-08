91f00a371b72e1ecf017b51b29e8b3ad05d5b69a
Jon <jhharwood88@gmail.com>
2019-06-08 15:16:39 -0400

    added validations for username to new user form, validates presence and unqieness

--- Shopping-List/app/models/user.rb
+++ Shopping-List/app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: %i[github]
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
 
     has_many :lists
     has_many :items, through: :lists
