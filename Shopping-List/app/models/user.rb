99844024bc25f180de74ca0b85a7c779d817c485
Jon <github email address>
2019-09-12 17:24:04 -0400

    removed commneted code

--- Shopping-List/app/models/user.rb
+++ Shopping-List/app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
 
 	def self.from_omniauth(auth)
 	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	  	
 	    user.email = auth.info.email
 	    user.password = Devise.friendly_token[0, 20]
 	    user.username = auth.info.nickname
@@ -22,14 +23,6 @@ class User < ApplicationRecord
 	    # user.skip_confirmation!
 	  end
 	end
-
-	# def self.new_with_session(params, session)
- #    super.tap do |user|
- #      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
- #        user.email = data["email"] if user.email.blank?
- #      end
- #    end
- #  end
 
 end
 
