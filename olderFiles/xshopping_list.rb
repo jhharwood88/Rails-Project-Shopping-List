2beec7db42548d792e824ad62b190d83e8066602
Jon <github email address>
2019-09-12 17:31:38 -0400

    Moved README to main folder to display in github, removed 'oldFiles' folder from repo

--- olderFiles/xshopping_list.rb
+++
@@ -1,8 +0,0 @@
-class xShoppingList < ApplicationRecord
-	belongs_to :user
-	has_many :item_lists
-	has_many :items, through: :item_lists
-
-	validates :name, presence: true
-	validates :user_id, presence: true
-end
